33de5e3f8332db80a4868cf4cae1fd2d87ddf934
Irene Fernandes <fernandesm.irene@gmail.com>
2019-09-26 21:48:54 +0100

    added retreat validations

--- app/models/retreat.rb
+++ app/models/retreat.rb
@@ -2,4 +2,9 @@ class Retreat < ApplicationRecord
   belongs_to :thread_admin
   has_many :users, through: :enrollments
   has_many :enrollments, dependent: :destroy
+
+  validates :name, presence: true, uniqueness: true
+  validates :location, presence: true
+  validates :description, presence: true, length: { maximum: 500 }
+  validates :price, presence: true, numericality: { greater_than: 1, less_than_or_equal_to: 999, only_integer: true }
 end
